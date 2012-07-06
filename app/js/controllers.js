9b2e96e1d389fc903e7cfdd2ce9760876949583a
Nat Weerawan <nat.wrw@gmail.com>
2012-07-06 18:45:36 +0700

    Add hangmanController

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,11 +2,33 @@
 
 /* Controllers */
 
+function HangmanController($scope, $log, $routeParams) {
+	$scope.abc = "abcdefghijklmnopqrstuvwxyz".toUpperCase().split('');
+	$scope.level = $routeParams.level || 0;
+  $scope.canvasId = "stage";
+  $scope.count = 0;
+  $scope.nat = "Nat";
 
-function MyCtrl1() {}
-MyCtrl1.$inject = [];
+
+  // Construct
+  !function() {
+    var t = new Array($scope.level*3);
+
+    $scope.word = t.join('_');
+    if ($scope.level < 1 ) {
+    	window.location = '#/level';
+    }
+  }();
 
 
-function MyCtrl2() {
+	$scope.guess = function(char) {
+    $scope.nat = "XNat";
+    $scope.count++;
+		$log.log("GUESSNG ", char, "in level", $scope.level, $scope.word, $scope.count);
+	}
+
+	$scope.chooseLevel = function(level) {
+		$scope.level = level;
+		window.location = '#/game/'+level;
+	}
 }
-MyCtrl2.$inject = [];
