ec8de36d0fd6c2ead7b371133e13633c7b9f2aca
Nat Weerawan <nat.wrw@gmail.com>
2012-07-06 18:40:28 +0700

    Config routeProvider

--- app/js/app.js
+++ app/js/app.js
@@ -4,7 +4,10 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: MyCtrl1});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: MyCtrl2});
-    $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.
+      when('/view1', {templateUrl: 'partials/partial1.html',   controller: MyController}).
+      when('/level', {templateUrl: 'partials/choose_level.html',   controller: HangmanController}).
+      //when('/game', {templateUrl: 'partials/game.html',   controller: HangmanController}).
+      when('/game/:level', {templateUrl: 'partials/game.html',   controller: HangmanController}).
+      otherwise({redirectTo: '/level'});
   }]);
