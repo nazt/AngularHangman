0f3a32dc23256fe60351c99414f93cd3053c51fb
Nat Weerawan <nat.wrw@gmail.com>
2012-07-09 16:53:14 +0700

    Remove route to view1

--- app/js/app.js
+++ app/js/app.js
@@ -5,7 +5,6 @@
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
   config(['$routeProvider', function($routeProvider) {
   $routeProvider.
-      when('/view1', {templateUrl: 'partials/partial1.html',   controller: MyController}).
       when('/level', {templateUrl: 'partials/choose_level.html',   controller: HangmanController}).
       //when('/game', {templateUrl: 'partials/game.html',   controller: HangmanController}).
       when('/game/:level', {templateUrl: 'partials/game.html',   controller: HangmanController}).
