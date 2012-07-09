062b373805e7e7bf5b534d0246987a6dcb3e3b60
Nat Weerawan <nat.wrw@gmail.com>
2012-07-09 17:23:27 +0700

    add hangmanBrain

--- app/js/services.js
+++ app/js/services.js
@@ -5,5 +5,20 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
+angular.module('hangman.services', []).
+  value('version', '0.1').factory('hangmanBrain', function() {
+    return new function() {
+        var self = this,
+            mappedVal = function(w, char) {  return (_.map(w, function(v, k) { if (v.toLowerCase() == char.toLowerCase()) { return k } })) },
+            stripUndefinded  = function(l) { return  _.reject(l, function(v) { return v === undefined; }) },
+            findIdx = _.compose(stripUndefinded, mappedVal);
+            
+        this.guess = findIdx;
+
+        this.preparedAnswerObj = function(w) {
+          var g = {};
+          _.each(w, function(v, k) { g[v] = findIdx(w, v)});
+          return g;
+        }
+    }
+});;
