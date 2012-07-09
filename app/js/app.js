e911e6ba18281355fbb34141645d6785f13fa009
Nat Weerawan <nat.wrw@gmail.com>
2012-07-09 17:23:14 +0700

    Change module name

--- app/js/app.js
+++ app/js/app.js
@@ -2,11 +2,11 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
+angular.module('hangman', ['hangman.filters', 'hangman.services', 'hangman.directives']).
   config(['$routeProvider', function($routeProvider) {
   $routeProvider.
-      when('/level', {templateUrl: 'partials/choose_level.html',   controller: HangmanController}).
+      when('/level', {templateUrl: 'partials/choose_level.html', controller: HangmanController}).
       //when('/game', {templateUrl: 'partials/game.html',   controller: HangmanController}).
-      when('/game/:level', {templateUrl: 'partials/game.html',   controller: HangmanController}).
+      when('/game/:level', {templateUrl: 'partials/game.html', controller: HangmanController}).
       otherwise({redirectTo: '/level'});
   }]);
