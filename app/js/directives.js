34c2905eaeb588b07be97945d8308da4bd7b2f1a
Nat Weerawan <nat.wrw@gmail.com>
2012-07-10 16:23:32 +0700

    custom directive.

--- app/js/directives.js
+++ app/js/directives.js
@@ -1,11 +1,24 @@
 'use strict';
 
 /* Directives */
+var hangmanDirectives = angular.module('hangman.directives', []);
 
-
-angular.module('hangman.directives', []).
-  directive('appVersion', ['version', function(version) {
+hangmanDirectives.directive('appVersion', ['version', function(version) {
     return function(scope, elm, attrs) {
       elm.text(version);
     };
-  }]);
+}]);
+
+hangmanDirectives.directive('myClick', ['$parse', function($parse) {
+	return function (scope, iElement, iAttrs) {
+		var fn = $parse(iAttrs.myClick);
+		iElement.bind('click', function(evt){
+			var self = this;
+			scope.clickVar = 'clicked';
+			scope.$apply(function() { fn(scope, {$event: evt}); });
+			iElement.unbind();
+		});
+	};
+}]);;
+
+
