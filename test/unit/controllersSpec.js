9492639ce679a4c3cc0b98a78ff058a6960316d9
Nat Weerawan <nat.wrw@gmail.com>
2012-07-11 16:57:19 +0700

    Add chooseLevel test.

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -2,30 +2,43 @@
 
 /* jasmine specs for controllers go here */
 
-describe('MyCtrl1', function(){
-  var myCtrl1;
+describe('Hangman', function () {
+  var scope,
+      window;
+      
+  describe('LevelController', function(){
+    beforeEach(inject(function($controller, $rootScope, $window) {
+      // store reference to scope, so that we can access it from the specs
+      scope = $rootScope.$new();
+      window = { };
 
-  beforeEach(function(){
-    myCtrl1 = new MyCtrl1();
+      // instantiate the controller
+      $controller('LevelController', {$scope: scope, $window: window});
+    }));
+
+    describe('allowLevel', function () {
+      it('should equal to [1,2,3]',function () {
+        expect(scope.allowLevel).toEqual([1, 2, 3]);
+      });
+    });
+
+    describe('choose valid level', function () {
+      it('should equal', function () {
+        angular.forEach([1, 2, 3], function(l){
+          scope.chooseLevel(l);
+          expect(window.location).toBe('#/game/'+l);
+        });
+      });
+    })
+
+    describe('choose invalid level', function () {
+      it('should equal', function () {
+        angular.forEach([4, 5, 6, 'a', undefined, ''], function(l){
+          scope.chooseLevel(l);
+          expect(window.location).toBe('#/level');
+        });
+      });
+    })
   });
 
-
-  it('should ....', function() {
-    //spec body
-  });
-});
-
-
-describe('MyCtrl2', function(){
-  var myCtrl2;
-
-
-  beforeEach(function(){
-    myCtrl2 = new MyCtrl2();
-  });
-
-
-  it('should ....', function() {
-    //spec body
-  });
-});
+})
