9cc9f4337cf45df461eef5a63625476b5d38b1de
Nat Weerawan <nat.wrw@gmail.com>
2012-07-12 15:23:41 +0700

    Add test and remove service

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,56 +2,97 @@
 
 /* Controllers */
 
-function GameController($scope, $log, $routeParams, hangmanBrain) {
-    var maxWrong = 10;
-    var dictionaries = {1: ['ant', 'rat', 'bat', 'hat', 'sat'], 2: ['worship', 'warlord', 'daddy'], 3: ['opendream', 'agility', 'preparing'] }
+function GameController($scope, $log, $routeParams) {
+    $scope.maxWrong = 10;
+    $scope.dictionaryProvider = (function() { 
+        var dicts = {
+            1: ['ant', 'rat', 'bat', 'hat', 'sat'], 
+            2: ['warship', 'warlord', 'daddy'],
+            3: ['opendream', 'agility', 'preparing']
+        }
+        var ret = {};
+        ret.getWord = function(level) {
+            return (_.shuffle(dicts[level])[0]).toUpperCase();
+        }
+        return ret;
+    })();
+
     $scope.abc = "abcdefghijklmnopqrstuvwxyz".toUpperCase().split('');
-    $scope.question = _.shuffle(dictionaries[$routeParams.level])[0]
-    $scope.level = $scope.question.length || $routeParams.level || 0;
+    $scope.used = {};
     $scope.wrong = 0;
-    $scope.guessed = [];
     $scope.correct = 0;
     $scope.gameStatus = '';
+    $scope.level = $routeParams.level;
+    $scope.guessed = [];
 
-    // Construct
-    !function() {
-        $scope.word = new Array($scope.level+1).join('_');
+
+    // INIT 
+
+    $scope.setWord = function() {
+        $scope.question = $scope.dictionaryProvider.getWord($routeParams.level);
+        $scope.word = new Array($scope.question.length+1).join('_');
         $scope.revealedWord = $scope.word.split('');
-        if ($scope.level < 1 ) {
-            window.location = '#/level';
-        }
-    }();
+    }
+    
+    $scope.setWord();
+    
+    $scope.findAllIdx = function(search) {
+        var charList = $scope.question.split('');
+        var result = [];
 
-
-    $scope.guess = function(char) {
-        var idxes;
-        if ($scope.gameStatus !== '') { return; }
-        $scope.guessed.push(char);
-        idxes = hangmanBrain.guess($scope.question.split(''), char);
-        if (_.isEmpty(idxes)) {
-            $scope.wrong++;
-            if ($scope.wrong >= maxWrong) {
-                $scope.message = "You LOSE!";
-                $scope.gameStatus = "lose";
-                $scope.word = $scope.question;
-                return;
+        _.filter(charList, function(val, key) { 
+            if (val == search) {
+                result.push(key)
+                return true
             }
-        }
-        else {
-            $scope.correct++;
-            _.each(idxes, function(v, k){ 
-                $scope.revealedWord[v] = $scope.question[v];
-            });
-            var pp = hangmanBrain.preparedAnswerObj($scope.question.split(''));
-            if (_.size(pp) == $scope.correct) {
-                $scope.message = "You WIN!";
-                $scope.gameStatus = "win";
+            else {
+                return false;
             }
-            $scope.word = $scope.revealedWord.join('');
-        }
+        }); 
+        return result;
     }
 
 
+    $scope.guess = function(c) {
+        console.log('guess', c, 'of', $scope.question)
+        var idxes;
+
+        // RETURN IF CHAR USED
+        if (_.has($scope.used, c)) { 
+            return; 
+        }
+
+        // GUESSING
+        idxes = $scope.findAllIdx(c);
+        $scope.used[c] = true;
+
+
+        // CORRECT OR WRONG
+        if (_.isEmpty(idxes)) {
+            $scope.wrong += 1;
+        }
+        else {
+            _.each(idxes, function(idx){
+                $scope.revealedWord[idx] = $scope.question[idx];
+            });
+            $scope.correct += 1;
+        }
+
+        // REVEAL THE WORD
+        $scope.word = $scope.revealedWord.join('')
+
+
+        // WIN OR LOSE
+        if ($scope.wrong == $scope.maxWrong) {
+            $scope.gameStatus = 'lose'
+            $scope.message = 'You LOSE!'
+            $scope.word = $scope.question;
+        }
+        else if ($scope.word == $scope.question) {
+            $scope.gameStatus = 'win';
+            $scope.message = 'You WIN!';
+        }
+    }
 }
 
 function LevelController($scope, $window) {
@@ -64,7 +105,7 @@ function LevelController($scope, $window) {
         else {
             gameLevel = '#/level';  
         }
-        
+
         $window.location = gameLevel;
     }
 }
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -2,43 +2,170 @@
 
 /* jasmine specs for controllers go here */
 
-describe('Hangman', function () {
-  var scope,
-      window;
-      
-  describe('LevelController', function(){
-    beforeEach(inject(function($controller, $rootScope, $window) {
-      // store reference to scope, so that we can access it from the specs
-      scope = $rootScope.$new();
-      window = { };
+describe('Hangman Controllers', function () {
+    describe('LevelController', function() {
+        var scope,
+                window;
+        beforeEach(inject(function($controller, $rootScope, $window) {
+            // store reference to scope, so that we can access it from the specs
+            scope = $rootScope.$new();
+            window = { };
 
-      // instantiate the controller
-      $controller('LevelController', {$scope: scope, $window: window});
-    }));
+            // instantiate the controller
+            $controller('LevelController', {$scope: scope, $window: window});
+        }));
 
-    describe('allowLevel', function () {
-      it('should equal to [1,2,3]',function () {
-        expect(scope.allowLevel).toEqual([1, 2, 3]);
-      });
+        describe('allowLevel', function () {
+            it('should equal to [1,2,3]',function () {
+                expect(scope.allowLevel).toEqual([1, 2, 3]);
+            });
+        });
+
+        describe('choose valid level', function () {
+            it('should equal', function () {
+                angular.forEach([1, 2, 3], function(l){
+                    scope.chooseLevel(l);
+                    expect(window.location).toBe('#/game/'+l);
+                });
+            });
+        })
+
+        describe('choose invalid level', function () {
+            it('should equal', function () {
+                angular.forEach([4, 5, 6, 'a', undefined, ''], function(l){
+                    scope.chooseLevel(l);
+                    expect(window.location).toBe('#/level');
+                });
+            });
+        })
     });
 
-    describe('choose valid level', function () {
-      it('should equal', function () {
-        angular.forEach([1, 2, 3], function(l){
-          scope.chooseLevel(l);
-          expect(window.location).toBe('#/game/'+l);
-        });
-      });
-    })
 
-    describe('choose invalid level', function () {
-      it('should equal', function () {
-        angular.forEach([4, 5, 6, 'a', undefined, ''], function(l){
-          scope.chooseLevel(l);
-          expect(window.location).toBe('#/level');
+    describe('GameController', function () {
+        var scope,
+                window,
+                routeParams;
+
+        beforeEach(inject(function($controller, $rootScope, $window) {
+            // store reference to scope, so that we can access it from the specs
+            scope = $rootScope.$new();
+            //scope.question = 'daddy';
+            //scope.dictionaryProvider = { getWord: function(l) { return 'daddy'; } };
+            window = { };
+            routeParams = { level: 2 }
+            // instantiate the controller
+            $controller('GameController', { $scope: scope, 
+                $window: window, $routeParams: routeParams});
+        }));
+
+        describe("Initialize value", function () {
+            it("should Initialize value of abc", function () {
+                var ABC = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 
+                'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+                 'W', 'X', 'Y', 'Z' ];
+                expect(scope.abc).toEqual(ABC);
+            });
+
+            it("should Initialize all tries", function () {
+                expect(scope.wrong).toBe(0);
+                expect(scope.correct).toBe(0);
+                expect(scope.gameStatus).toBe('');
+                expect(scope.level).toBe(2);
+
+
+                //scope.guess('d');
+                //expect(scope.revealedWord).toEqual(['d','_', 'd', 'd', 'y']);
+                //expect(scope.wrong).toBe(0);
+                //expect(scope.correct).toBe(0);
+            });
         });
-      });
+
+        describe("Find All Idx", function () {
+            it("should return position correctly", function () {
+                scope.dictionaryProvider = { getWord: function(l){ return 'daddy'; } }
+                scope.setWord();
+                expect(scope.question).toBe('daddy');
+                expect(scope.word).toEqual("_____")
+                expect(scope.revealedWord).toEqual(['_', '_', '_', '_', '_']);
+
+                expect(scope.findAllIdx('d')).toEqual([0, 2, 3])
+                expect(scope.findAllIdx('a')).toEqual([1])
+                expect(scope.findAllIdx('y')).toEqual([4])
+            })  
+        });
+
+        describe("Guess a word and win game", function () {
+            it("should guess reveal correctly", function () {
+                scope.dictionaryProvider = { getWord: function(l){ return 'daddy'; } }
+                scope.setWord(); 
+
+                scope.guess('d');
+                expect(scope.word).toEqual("d_dd_")
+                expect(scope.revealedWord).toEqual(['d', '_', 'd', 'd', '_']);
+                expect(scope.correct).toBe(1);
+
+                scope.guess('d');
+                expect(scope.word).toEqual("d_dd_")
+                expect(scope.revealedWord).toEqual(['d', '_', 'd', 'd', '_']);
+                expect(scope.correct).toBe(1);
+                expect(scope.gameStatus).toBe('');
+
+                scope.guess('a');
+                expect(scope.word).toEqual("dadd_")
+                expect(scope.revealedWord).toEqual(['d', 'a', 'd', 'd', '_']);
+                expect(scope.correct).toBe(2);
+                expect(scope.gameStatus).toBe('');
+
+                scope.guess('y');
+                expect(scope.word).toEqual("daddy")
+                expect(scope.revealedWord).toEqual(['d', 'a', 'd', 'd', 'y']);
+                expect(scope.correct).toBe(3);
+                expect(scope.gameStatus).toBe('win');
+                expect(scope.message).toBe('You WIN!');
+           });
+        });
+
+        describe("Guess a word and lose game", function () {
+            it("should lose the game", function () {
+                scope.dictionaryProvider = { getWord: function(l){ return 'daddy'; } }
+                scope.setWord(); 
+                scope.maxWrong = 5;
+
+                scope.guess('d');
+                expect(scope.word).toEqual("d_dd_")
+                expect(scope.revealedWord).toEqual(['d', '_', 'd', 'd', '_']);
+                expect(scope.correct).toBe(1);
+                expect(scope.wrong).toBe(0);
+
+                scope.guess('p');
+                expect(scope.correct).toBe(1);
+                expect(scope.wrong).toBe(1);
+                expect(scope.gameStatus).toBe('');
+
+                scope.guess('q');
+                expect(scope.correct).toBe(1);
+                expect(scope.wrong).toBe(2);
+                expect(scope.gameStatus).toBe('');
+
+                scope.guess('r');
+                expect(scope.correct).toBe(1);
+                expect(scope.wrong).toBe(3);
+                expect(scope.gameStatus).toBe('');
+
+                scope.guess('s');
+                expect(scope.correct).toBe(1);
+                expect(scope.wrong).toBe(4);
+                expect(scope.gameStatus).toBe('');
+
+                scope.guess('t');
+                expect(scope.correct).toBe(1);
+                expect(scope.wrong).toBe(5);
+                expect(scope.gameStatus).toBe('lose');
+                expect(scope.message).toBe('You LOSE!');
+                expect(scope.word).toEqual('daddy');
+           });
+        });
+
     })
-  });
 
 })
